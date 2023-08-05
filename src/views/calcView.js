ff7a516422085170fce3d6510ebcd04cf15f492c
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-08-05 17:04:04 +0300

    Small fixes

--- src/views/calcView.js
+++ src/views/calcView.js
@@ -4,13 +4,15 @@ import {html, render} from "../lib.js";
 
 const catalogTemplate = (data) => html`
 ${data.length > 0 
-? data.map( food => html`<div class="card">
+? data.map( food => html`<div class="container">
+            <div class="card">
             <div>
             <h1>${food.description}</h1>
             <p style="text-transform: capitalize;"><strong>Calories :</strong> 24 g <strong>Fat :</strong> 13 g <Strong>Protein</Strong> 14 g <Strong>Carbs</Strong> 14 g</p>
             <div>
             <button class="add-btn">Calculate</button>
             </div>
+        </div>
         </div>
         </div>`) 
 : html`<h1>There is no data to show.</h1>`}`;
@@ -20,7 +22,7 @@ ${data.length > 0
 export const showCatalog = async () => {
     const data = await getData();
     console.log(data)
-    render(catalogTemplate(data), document.querySelector('main div.container'));
+    render(catalogTemplate(data), document.querySelector('main'));
 }
 
 async function getData () {
